58df0684f1e736701740054f6b7f091e5eae6e19
sl394@duke.edu <sl394@duke.edu>
2017-09-26 11:04:05 -0400

    all the unkwowns

--- data_util_lstm.py
+++ data_util_lstm.py
@@ -331,6 +331,8 @@ if __name__ == "__main__":
     # words in labels set as unknown
     unk_lb_word_g = [word for word in unk_g if word in w2i_sm]
     unk_lb_word_m = [word for word in unk_m if word in w2i_sm]
+    unk_word_g = [word for word in unk_g if word in word2idx]
+    unk_word_m = [word for word in unk_m if word in word2idx]
 
     # add special token to both the notes vocabulary and the semantic vocabulary
     word2idx,idx2word = add_special_token(word2idx,idx2word)
@@ -360,5 +362,5 @@ if __name__ == "__main__":
     lb_lst = dict(lb_lst)
     everything = [train, dev, test, Wemb_g, Wemb_m, idx2word, word2idx, i2w_lb, i2w_sm,ConfigInfo, dicts_mapping, (lb_lst,lb_freq_train,lb_freq_test)]
     cPickle.dump(everything, open('./data/lstm_everything' + affx + str(freq_lbd_idx) + '.p', "wb"))
-    cPickle.dump(everything[5:] + unk_lb_word_m + unk_lb_word_g, open('./data/lstm_no_dt' + str(freq_lbd_idx) + '.p', "wb"))
+    cPickle.dump(everything[5:] + unk_lb_word_m + unk_lb_word_g + unk_word_m + unk_word_g, open('./data/lstm_no_dt' + str(freq_lbd_idx) + '.p', "wb"))
 #    print "dataset created!"
